f39c5babe9fad30219a199098a824fe5143b0414
Albert Yu <92063396+albertyu03@users.noreply.github.com>
2022-01-19 03:21:48 +0000

    testing Car constructors

--- car.cpp
+++ car.cpp
@@ -4,4 +4,28 @@
 #include "car.hpp"
 #include <cstddef>
 #include <cstring>
-// #include .......
+Car::Car() {
+  this->manufacturer = NULL;
+  this->model = NULL;
+  this->zeroToSixtyNs = '0';
+  this->headonDragCoeff = '0';
+  this->horsepower = '0';
+  this->backseatDoors = '0';
+  this->seatCount = '0';
+
+}
+
+Car(char const* const manufacturerName, char const* const modelName, PerformanceStats perf, uint8_t numSeats, DoorKind backseatDoorDesign) {
+  this->manufacturer = manufacturerName;
+  this->modelName = modelName;
+  this->zeroToSixtyNs = perf->zeroToSixtyNs;
+  this->headonDragCoeff = perf->headonDragCoeff;
+  this->horsepower = perf->horsepower;
+  this->backseatDoors = backseatDoorDesign;
+  this->seatCount = numSeats;
+}
+/*
+Car::Car(Car const& o) {
+  this->manufacturer = *o->
+}                                                                          
+*/                                             ~                 
